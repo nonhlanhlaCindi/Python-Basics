12804157d5bb0d6421cb33a72c87319a85ca93eb
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-02-04 11:56:46 +0200

    display

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print('HELLO NONNY') #Greetings
+# print('HELLO NONNY') 
 
+#Variables
 name = input('What is your name? ')
 age = input('How old are you? ')
+
+print('Hey '+ name + ' Sweetheart!')
+print('You are ' + age)
