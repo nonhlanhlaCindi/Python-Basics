3130cf046b7c65654a367a5fdbbe19b9130a0e11
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-02-05 15:02:47 +0200

    variablea

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # print('HELLO NONNY') 
 
 #Variables
-name = input('What is your name? ')
-age = input('How old are you? ')
+name = str(input('What is your name? '))
+age = int(input('How old are you? '))
 
 print('Hey '+ name + ' Sweetheart!')
-print('You are ' + age)
+print('You are ' + str(age))
 
+
+print(f"{name}")
