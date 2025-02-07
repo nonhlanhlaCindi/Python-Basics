0e75a9105bddbc27e218093e6b16f1ca39a5bc7f
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-02-07 11:40:22 +0200

    Conditional statements

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-# print('HELLO NONNY') 
+# # print('HELLO NONNY') 
 
-#Variables
-name = str(input('What is your name? '))
-age = int(input('How old are you? '))
+# #Variables
+# name = str(input('What is your name? '))
+# age = int(input('How old are you? '))
 
-print('Hey '+ name + ' Sweetheart!')
-print('You are ' + str(age))
+# print('Hey '+ name + ' Sweetheart!')
+# print('You are ' + str(age))
 
 
-print(f"{name}")
+# print(f"{name}")
 
 
 #Loops
+#FLOOR CONTROL
+
+# age = 19
+# if age > 19:
+#         print('You are an adult')
+        
+# else:   print('You are a teenager')
+
+x = 2
+
+if x > 2:
+    print('you have gotten 23')
+    
+elif x == 5:
+    print('goten just 2')
+    
+else: x
+print('Such a low score')
+
