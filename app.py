e76cf6cae8d8a9f8f9497832036f534cd16e075a
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-02-07 14:06:29 +0200

    loops

--- app.py
+++ app.py
@@ -8,34 +8,36 @@
 # print('You are ' + str(age))
 
 
-# print(f"{name}")
+# # print(f"{name}")
 
 
-#Loops
-#FLOOR CONTROL
+# #Loops
+# #FLOOR CONTROL
 
 
-id = input('Do you have an IDENTITY DOCUMENT (True/False?) ')
-#The code id = bool(input('Do you have an IDENTITY DOCUMENT (True/False?) ')) 
-# is not behaving as expected because the input() function always returns a string, 
-# even if the user enters "True" or "False." When you convert this string to a boolean using bool(),
-# any non-empty string, including "True" or "False," will be evaluated as True.
-# Therefore, regardless of the input, the result will always be True. This is what's causing the unexpected behavior in this code.
+
+# #The code id = bool(input('Do you have an IDENTITY DOCUMENT (True/False?) ')) 
+# # is not behaving as expected because the input() function always returns a string, 
+# # even if the user enters "True" or "False." When you convert this string to a boolean using bool(),
+# # any non-empty string, including "True" or "False," will be evaluated as True.
+# # Therefore, regardless of the input, the result will always be True. This is what's causing the unexpected behavior in this code.
+
+# id = input('Do you have an IDENTITY DOCUMENT (True/False?) ')
+# age = int(input('How old are you? '))
+
+# bool(id)
 
 
-age = int(input('How old are you? '))
-
-bool(id)
-
-
-if id == True and age >= 21: 
-        print('You are welcome to CAVO BY SKHOTHENI')
+# if id == True and age >= 21: 
+#         print('You are welcome to CAVO BY SKHOTHENI')
         
-elif id == True and age < 21:
-    print('Go home and do your homework, you are still a child')
+# elif id == True and age < 21:
+#     print('Go home and do your homework, you are still a child')
     
-else:
-    print('Provide proof')
+# else:
+#     print('Provide proof')
+
+
 
 
 
@@ -50,3 +52,6 @@ else:
 # else: x
 # print('Such a low score')
 
+fruits = ['apple','banana','pineapple','watermelon']
+for fruit in fruits:
+        print(fruit)
